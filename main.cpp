f2326de0f647d26c9e39dc6dd6ded8e240c3926a
Nicole <nickybelu@gmail.com>
2024-11-04 01:07:13 -0300

    Menu

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 
-    sf::RenderWindow ventana(sf::VideoMode(800, 600), "Rescate mágico: El rescate del huevo del caballero");
+    sf::RenderWindow ventana(sf::VideoMode(800, 600), "Rescate magico: El rescate del huevo del caballero");
     Menu menuPrincipal;
 
     while(ventana.isOpen()){
