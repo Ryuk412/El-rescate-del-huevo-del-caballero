13455290a1de404ef5da94cc242adec6f05e8eda
Dylan <gerberdylan11@gmail.com>
2024-11-04 04:19:42 -0300

    Revert "cosas"
    This reverts commit 7eb8ae1ff58de38f00eb0cc4a46618cd812a4e63.

--- src/enemigo.cpp
+++ src/enemigo.cpp
@@ -2,33 +2,10 @@
 #include "enemigo.h"
 
 
-enemigo::enemigo( )
+enemigo::enemigo()
 {
 
-_nEnemigo=1;
-
-
-
-}
-
-void enemigo::setEnemigo( ){
-
-
-   switch (_nEnemigo)
-   {
-   case 1:
-        _textureE.loadFromFile("assets/Characters(100x100)/Slime/Slime/Slime.png");
-       break;
-   case 2:
-        _textureE.loadFromFile("assets\Characters(100x100)\Orc rider\Orc rider.png");
-    break;
-
-   default:
-       break;
-   }
-
-
-
+    _textureE.loadFromFile("assets/Characters(100x100)/Slime/Slime/Slime.png");
     e_sprite.setTexture(_textureE);
     e_sprite.setTextureRect({0,0,130,130});
     e_sprite.setOrigin(e_sprite.getGlobalBounds().width/2, e_sprite.getGlobalBounds().height);
