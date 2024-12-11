d935d5517c2f2f0847be52d08600ab1aa6ef762b
Nicole <nickybelu@gmail.com>
2024-12-11 00:55:57 -0300

    .

--- src/Final.cpp
+++ src/Final.cpp
@@ -1,37 +1,40 @@
 #include "Final.h"
 #include "Menu.h"
 
-void Final::finalMenu(sf::RenderWindow& window) {
+void Final::finalMenu(sf::RenderWindow& window){
+    Boton empezar(273, 496, 255, 30, " ");
+
     sf::Texture texture;
-    if (!texture.loadFromFile("menu/final.png")) {
-        return;  // Si no se puede cargar la textura, no se muestra la pantalla final
-    }
+    if (!texture.loadFromFile("menu/final.png")){return;}
 
-    bool salirAMenu = false;
+    bool salirAMenu=0;
 
-    while(window.isOpen()){
+    while (window.isOpen()){
         sf::Event event;
 
-        while(window.pollEvent(event)){
+        while (window.pollEvent(event)){
+
             if(event.type == sf::Event::Closed){
                 window.close();
             }
 
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-                salirAMenu=true;
+            if(event.type == sf::Event::MouseButtonPressed){
+                salirAMenu=1;
                 window.close();
             }
+
         }
 
-        window.setView(window.getDefaultView());
         window.clear();
         sf::Sprite sprite(texture);
         window.draw(sprite);
+
         window.display();
+
     }
 
     if(salirAMenu){
         menu();
     }
-}
 
+}
